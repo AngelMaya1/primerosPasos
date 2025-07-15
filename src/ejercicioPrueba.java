3b880eeda024365c73b09a836483c7069109b95f
AngelMaya1 <luisangelmaya@192.168.1.190>
2025-07-15 01:02:43 -0600

    ejercicioPrueba1

---
+++ src/ejercicioPrueba.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ejercicioPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numeroAleatorio=(int)(Math.random()*100)+1;
+		
+		int intentos=0;
+		
+		int numeroUno=numeroAleatorio;
+		
+		int numeroDos=0;
+		
+		while(numeroUno!=numeroDos) {
+		
+			intentos++;
+			
+			numeroDos=Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero del 1 al 100"));
+			
+			if(numeroDos<numeroUno)System.out.println("El numero ingresado es menor");
+			
+			else if(numeroDos>numeroUno)System.out.println("El numero ingresado es mayor");
+		}
+		
+		System.out.println("Bravo adivinastes el numero");
+		
+		System.out.println("Te a tomado " + intentos + " intentos para adivinar el numero");
+	}
+
+}
